e9fb649679907ef883ccea1652a2deb2179d67b6
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-09-03 15:03:07 +0200

    import third_party/uriparser

--- cmake/FindUriParser.cmake
+++ cmake/FindUriParser.cmake
@@ -5,31 +5,18 @@
 #  URIPARSER_LIBRARY     - uriparser library file
 #  URIPARSER_FOUND       - TRUE if uriparser is found
 
-if (URIPARSER_INCLUDE_DIR)
- #check cache 
-  set(URIPARSER_FIND_QUIETLY TRUE)
-endif ()
-
-if (NOT URIPARSER_INCLUDE_DIR)
-    find_path(URIPARSER_INCLUDE_DIR NAMES Uri.h PATH_SUFFIXES uriparser)
-    set(URIPARSER_INCLUDE_DIR ${URIPARSER_INCLUDE_DIR}/uriparser CACHE PATH "uriparser includes")
-endif ()
+set(URIPARSER_FOUND FALSE)
+find_path(URIPARSER_INCLUDE_DIR NAMES Uri.h PATH_SUFFIXES uriparser)
+set(URIPARSER_INCLUDE_DIR ${URIPARSER_INCLUDE_DIR}/uriparser CACHE PATH "uriparser includes")
 
 find_library(URIPARSER_LIBRARY NAMES uriparser)
-
 if (URIPARSER_INCLUDE_DIR AND URIPARSER_LIBRARY)
   set(URIPARSER_FOUND TRUE)
-  set(UriParser_FOUND TRUE)
 endif ()
 
-if (URIPARSER_FOUND)
-   if (NOT URIPARSER_FIND_QUIETLY)
-      message(STATUS "Found UriParser library: ${URIPARSER_LIBRARY}")
-   endif ()
-else ()
-   if (NOT URIPARSER_FIND_QUIETLY)
-      message(FATAL_ERROR "Could NOT find UriParser library")
-    else ()
-      message(STATUS "Could NOT find UriParser library")
-    endif ()
-endif ()
+if(NOT URIPARSER_FIND_QUIETLY)
+  include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
+  find_package_handle_standard_args(URIPARSER 
+    REQUIRED_VARS URIPARSER_LIBRARY URIPARSER_INCLUDE_DIR
+    VERSION_VAR URIPARSER_VERSION_STRING)
+endif()
